6b83c11fc74d352238dd2a9f37d424d812833dc9
reve74 <reve99@naver.com>
2023-10-17 15:14:38 +0900

    메인화면 TableView 작업 진행

--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -9,19 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar! {
+        didSet {
+            searchBar.placeholder = "Search"
+            searchBar.tintColor = UIColor.yellow
+        }
+    }
+    
+    
     @IBOutlet weak var titleLabel: UILabel! {
         didSet {
             titleLabel.font = .systemFont(ofSize: 24, weight: .bold)
         }
     }
     
+    @IBOutlet weak var pokemonTableView: UITableView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        pokemonTableView.delegate = self
+        pokemonTableView.dataSource = self
+        searchBar.delegate = self
+        self.searchBar.searchBarStyle = .minimal
+
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PokemonCell", for: indexPath) as! PokemonCell
+        
+        return cell
+    }
+}
+
+extension ViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+          
+    }
+}
