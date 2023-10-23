61d451c5d412eae37f841b5bd8a99aa670786133
reve74 <revec99@naver.com>
2023-10-23 20:39:47 +0900

    [Function] DetailViewController API 데이터 파싱 진행중

--- Pokedex/DetailView/DetailViewController.swift
+++ Pokedex/DetailView/DetailViewController.swift
@@ -6,17 +6,19 @@
 //
 
 import UIKit
+import Alamofire
 
 class DetailViewController: UIViewController {
-    
-    
-//    var pokemonResult: 
-    
-    var pokemon: Pokemon?
-    
+        
     var img: UIImage?
     
-//    var pokemonAbilityView = PokemonAbilityView()
+    var index: Int?
+    
+    var pokemonName = ""
+    
+    var type: [String]?
+    
+    
     
     @IBOutlet weak var pokemonImageView: UIImageView!
     
@@ -34,30 +36,107 @@ class DetailViewController: UIViewController {
     
     
     @IBOutlet weak var pokemonType: PokemonAbilityView!
-    
+
     @IBOutlet weak var pokemonHeight: PokemonAbilityView!
-    
+
     @IBOutlet weak var pokemonWeight: PokemonAbilityView!
-    
+
     @IBOutlet weak var pokemonAbility: PokemonAbilityView!
     
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var widghtLabel: UILabel!
+    @IBOutlet weak var abilityLabel: UILabel!
+    
+    
+    
+    
+    func loadImage(urlString: String, completion: @escaping (UIImage?)-> Void){
+        let sessionConfig = URLSessionConfiguration.default
+        let session = URLSession(configuration: sessionConfig)
+        
+        if let hasURL = URL(string: urlString) {
+            var request = URLRequest(url: hasURL)
+            request.httpMethod = "GET"
+            
+            session.dataTask(with: request) { data, response, error in
+                print((response as! HTTPURLResponse).statusCode)
+                
+                if let hasData = data {
+                    completion(UIImage(data: hasData))
+                    return
+                }
+            }.resume()
+            session.finishTasksAndInvalidate()
+        }
+        completion(nil)
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let hasImage = pokemon?.image {
-          img =  UIImage(named: "\(hasImage).jpg")
-        }
-        pokemonImageView?.image = img
-        
-        if let hasNumber = pokemon?.number {
-            numberLabel.text = "No.\(String(hasNumber))"
+        guard let hasIndex = index else {
+            return
         }
         
-        if let hasName = pokemon?.name {
-            nameLabel.text = hasName
+        requestAPI(index: String(hasIndex))
+        
+        self.loadImage(urlString: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/\(hasIndex).png") { image in
+            DispatchQueue.main.async {
+                self.pokemonImageView?.image = image
+            }
         }
+        
+        numberLabel.text = "No.\(String(hasIndex))"
+        nameLabel.text = pokemonName
 
+    }
+    
+    func requestAPI(index: String) {
+        var request = AF.request("https://pokeapi.co/api/v2/pokemon/\(index)", method: .get).responseJSON {
+            response in
+
+            print("Result: \(String(describing: response.result))")
+            
+            switch response.result {
+            case .success:
+                guard let result = response.data else {return}
+
+                do {
+                    let decoder = JSONDecoder()
+                    let json = try decoder.decode(PokemonModel.self, from: result)
+                    
+                    print("json \(json)")
+
+                    print("type : \(json.types[0].type.name)")
+//                    print("type : \(json.types[1].type.name)")
+                    
+                    
+                    for i in 0..<json.types.count {
+                        self.type?.append(json.types[i].type.name)
+                        
+                        print(self.type)
+                        
+//                        result.append(try transform(self[i]))
+//                        formIndex(after: &i)
+                      }
+                    
+                    
+                    let height = json.height
+                    let weight = json.weight
+                    
+                    self.heightLabel.text = String(height)
+                    self.widghtLabel.text = String(weight)
+                    
+                    
+                } catch {
+                    print("error!\(error)")
+                }
+            default:
+                return
+            }
+            
+        }
     }
 }
